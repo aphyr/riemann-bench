f04a54b64d393dc0aa1fc5431016d6258fa5f582
Aphyr <aphyr@aphyr.com>
2013-01-09 16:27:22 -0800

    Use Schadenfreude

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject riemann-bench "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[riemann "0.0.3-SNAPSHOT"]
-                 [org.clojure/clojure "1.2.0"]
+  :description "Benchmark Riemann"
+  :dependencies [[riemann-clojure-client "0.0.6"]
+                 [me.raynes/conch "0.4.0"]
+                 [org.clojure/clojure "1.4.0"]
+                 [schadenfreude "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.logging "0.2.3"]
+                 [org.slf4j/slf4j-log4j12 "1.7.2"]
                 ]
   :aot [riemann-bench.core]
   :main riemann-bench.core
--- src/riemann_bench/core.clj
+++ src/riemann_bench/core.clj
@@ -1,43 +1,116 @@
 (ns riemann-bench.core
-  (:use riemann.client)
-  (:use riemann.logging)
-  (:use riemann.common)
-  (:use clojure.tools.logging)
+  (:use riemann.client
+        [clojure.stacktrace :only [print-cause-trace]]
+        [schadenfreude.git :only [compare-versions]]
+        clojure.java.shell)
+  (:require [conch.core :as sh])
   (:gen-class))
 
-(riemann.logging/init)
+(defn expand-path
+  [path]
+  (.getCanonicalPath (java.io.File. path)))
 
-(defmacro rtime
-    "Evaluates expr and returns the time it took in seconds"
-    [expr]
-    `(let [start# (. System (nanoTime))
-                    ret# ~expr]
-            (/ (- (. System (nanoTime)) start#) 1000000000.0)))
+(defn pid
+  "Get the PID of a process. Take a step back from the Java IPC APIs, and
+  literally FUCK YOUR OWN FACE."
+  [p]
+  (let [f (.. p (getClass) (getDeclaredField "pid"))]
+    (.setAccessible f true)
+    (.get f p)))
+
+(defn system
+  "Starts a process with cwd dir. Returns a conch process."
+  [& commands]
+  (let [p (apply sh/proc commands)]
+    (future (sh/stream-to-out p :out))
+    (future (sh/stream-to-out p :err))
+    p))
+
+(defn sh-now
+  [& args]
+  (sh/stream-to-string (apply sh/proc args) :out))
+
+(defn kill-children
+  "Send a signal to all children of process"
+  ([process] (kill-children :term process))
+  ([signal process]
+   (sh/proc "pkill" "--signal" (name signal) "-P" (str (pid process)))))
+
+(defn start-server
+  "Starts a riemann server from dir. Returns a conch Process."
+  [dir]
+  (let [p (system "lein" "do" "clean," "run" "--" 
+                  (expand-path "riemann.config") :dir dir)]
+    ; Spin until started.
+    (println "Waiting for server...")
+    (loop []
+      (if
+        (try
+          (close-client (tcp-client))
+          true
+          (catch java.net.ConnectException e false))
+        (println "Server ready.")
+        (do
+          (Thread/sleep 100)
+          (recur))))
+    p))
+
+(defn stop-server
+  "Stop a riemann server."
+  [process]
+  (let [code (future (sh/exit-code process))]
+    (kill-children :term (:process process))
+    (println "Server exited with" @code)))
+
+(def drop-tcp-run
+  {:name "drop tcp"
+   :n 500000
+   :threads 1
+   :before (fn [x]
+             (if-let [client (try
+                               (tcp-client)
+                               (catch java.net.ConnectException e
+                                 (print-cause-trace e)
+                                 nil))]
+               client
+               (do
+                 (Thread/sleep 100)
+                 (recur x))))
+   :f #(send-event % {:host "test"
+                      :service "drop tcp"
+                      :state "ok"
+                      :description "a benchmark"
+                      :metric 1.23
+                      :ttl 1
+                      :tags ["bench"]})
+   :after #(close-client %)})
+
+(defn suite
+  "A test suite against a riemann repo in dir."
+  [dir]
+  {:before #(start-server dir)
+   :runs [drop-tcp-run]
+   :after stop-server})
+
+(defn suite'
+  "A dumb debugging suite"
+  [dir]
+  {:before #(prn "setup")
+   :after #(prn "teardown" %)
+   :runs [{:name "hi"
+           :n 1000
+           :threads 1
+           :before #(prn "before" %)
+           :after #(prn "after" %)
+           :f (fn [_] (Thread/sleep 10))}]})
 
 (defn -main
-  [& argv]
-    (info "here")
-
-  (let [c (riemann.client/tcp-client)
-        n 1000
-        threads 100
-        events (take n (repeatedly 
-                         (fn []
-                           {:host "test"
-                            :service "bench"
-                            :state "ok"
-                            :description "Yo, I'm text!"
-                            :metric 1.23
-                            :ttl 1
-                            :tags ["bench"]})))]
-
-    (info 
-      (rtime
-        (threaded threads
-                  (let [client (tcp-client)]
-                    (doseq [e events] (send-event c e))
-                    (close-client client)))))
-
-    (info (query c "true"))
-    (close-client c))
-  (System/exit 0))
+  [dir & versions]
+  (try
+    (compare-versions dir versions (suite dir))
+    (flush)
+    (System/exit 0)
+    (catch Throwable t
+      (print-cause-trace t)
+      (flush)
+      (System/exit 1))))
