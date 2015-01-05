553721c45f484e435151dc240bd3b1894d94bcfc
Aphyr <aphyr@aphyr.com>
2015-01-04 23:14:16 -0800

    Mucking around with riemann-clojure-client 0.3.0

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
 (defproject riemann-bench "1.0.0-SNAPSHOT"
   :description "Benchmark Riemann"
-  :dependencies [[riemann-clojure-client "0.2.6"]
+  :dependencies [[riemann-clojure-client "0.3.0-SNAPSHOT"]
                  [me.raynes/conch "0.4.0"]
-                 [org.clojure/clojure "1.4.0"]
-                 [schadenfreude "0.1.1-SNAPSHOT"]
+                 [org.clojure/clojure "1.6.0"]
+                 [schadenfreude "0.1.2-SNAPSHOT"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.slf4j/slf4j-log4j12 "1.7.2"]
-                ]
-  :aot [riemann-bench.core]
+                 [org.slf4j/slf4j-log4j12 "1.7.2"]]
+  :global-vars {*warn-on-reflection* true}
+  :jvm-opts ["-server"
+             "-Dcom.sun.management.jmxremote"
+             "-XX:+UnlockCommercialFeatures"
+             "-XX:+FlightRecorder"]
+;  :aot [riemann-bench.core]
   :main riemann-bench.core
 )
--- src/riemann_bench/core.clj
+++ src/riemann_bench/core.clj
@@ -1,17 +1,18 @@
 (ns riemann-bench.core
   (:use riemann.client
-        [riemann.codec :only [map->Event]]
         [clojure.stacktrace :only [print-cause-trace]]
         [schadenfreude.git :only [compare-versions]]
         clojure.java.shell)
-  (:require [conch.core :as sh])
+  (:require [conch.core :as sh]
+            [clojure.java.io :as io]
+            [riemann.codec :as codec])
   (:import (java.util.concurrent LinkedBlockingQueue
-                                 ArrayBlockingQueue))
-  (:gen-class))
+                                 ArrayBlockingQueue)))
+;  (:gen-class))
 
 (defn expand-path
-  [path]
-  (.getCanonicalPath (java.io.File. path)))
+  [^String path]
+  (.getCanonicalPath (io/file path)))
 
 (defn pid
   "Get the PID of a process. Take a step back from the Java IPC APIs, and
@@ -39,23 +40,28 @@
   ([signal process]
    (sh/proc "pkill" "--signal" (name signal) "-P" (str (pid process)))))
 
+(defn await-tcp-server
+  []
+  (print "Waiting for server")
+  (flush)
+  (loop []
+    (when-not (with-open [c (tcp-client)]
+                (try
+                  @(query c "false")
+                  true
+                  (catch java.io.IOException e
+                    (print ".")
+                    (flush)
+                    false)))
+      (Thread/sleep 1000)
+      (recur))))
+
 (defn start-server
   "Starts a riemann server from dir. Returns a conch Process."
   [dir]
-  (let [p (system "lein" "do" "clean," "run" "--" 
+  (let [p (system "lein" "do" "clean," "run" "--"
                   (expand-path "riemann.config") :dir dir)]
-    ; Spin until started.
-    (println "Waiting for server...")
-    (loop []
-      (if
-        (try
-          (close-client (tcp-client))
-          true
-          (catch java.net.ConnectException e false))
-        (println "Server ready.")
-        (do
-          (Thread/sleep 100)
-          (recur))))
+    (await-tcp-server)
     p))
 
 (defn stop-server
@@ -65,111 +71,50 @@
     (kill-children :term (:process process))
     (println "Server exited with" @code)))
 
-(defn await-tcp-server
-  []
-  (print "Waiting for server")
-  (flush)
-  (loop []
-    (when-not (let [c (tcp-client)]
-                (try
-                  (query c "false")
-                  true
-                  (catch java.io.IOException e
-                    (print ".")
-                    (flush)
-                    false)
-                  (finally
-                    (close-client c))))
-      (Thread/sleep 500)
-      (recur))))
 
 (defn example-event
   []
-  (map->Event
-    {:host "test"
-     :service "async tcp rate run"
-     :state "ok"
-     :description "a benchmark"
-     :metric 1
-     :ttl 1
-     :tags ["bench"]}))
+  {:host "test"
+   :service "async tcp rate run"
+   :state "ok"
+   :description "a benchmark"
+   :metric 1
+   :ttl 1
+   :time (/ (System/currentTimeMillis) 1000)
+   :tags ["bench"]})
 
 (def bulk-async-tcp-rate-run
-  (let [events (take 100 (repeatedly #(example-event)))]
+  (let [msg (codec/encode-pb-msg
+              {:events (take 100 (repeatedly example-event))})]
     {:name "bulk async tcp rate"
-     :n 100000
-     :sample 10
-     :threads 4
+     :n 1000000
+     :threads 48
      :before (fn [x]
-               (await-tcp-server)
-               (let [n 5000
+               (let [n 10000
                      queue (ArrayBlockingQueue. n)
                      client (multi-client
-                              (take 2 (repeatedly #(tcp-client))))]
-;                     client (tcp-client)]
+                              (take 4 (repeatedly #(tcp-client))))]
+
                  ; Fill queue
                  (dotimes [i n]
-                   (.put queue (doto (promise) (deliver nil))))
+                   (.put queue (send-msg client msg)))
+
                  [queue client]))
-     :f (fn [[queue client]]
-          (try 
-            ; Block on a result
-            (deref (.poll queue))
+     :f (fn [[^ArrayBlockingQueue queue client]]
+          ; Block on a result
+          @(.poll queue)
 
-            ; Add a new write.
-            (.put queue (async-send-events client events))
-            (catch Exception e (println e))))
-     :after (fn [[queue c]]
-              (close-client c))}))
-
-(def async-tcp-rate-run
-  (let [event (example-event)]
-    {:name "async tcp rate"
-     :n 10000000
-     :sample 100
-     :threads 3
-     :before (fn [x]
-               (await-tcp-server)
-               (let [n 5000
-                     queue (ArrayBlockingQueue. n)
-;                     client (multi-client
-;                              (take 2 (repeatedly #(tcp-client))))]
-                     client (tcp-client)]
-                 ; Fill queue
-                 (dotimes [i n]
-                   (.put queue (doto (promise) (deliver nil))))
-                 [queue client]))
-     :f (fn [[queue client]]
-          (try 
-            ; Block on a result
-            (deref (.poll queue))
-
-            ; Add a new write.
-            (.put queue (async-send-event client event))
-            (catch Exception e (println e))))
-     :after (fn [[queue c]]
-              (close-client c))}))
+          ; Add a new write.
+          (.put queue (send-msg client msg)))
+     :after (fn [[_ c]]
+              (close! c))}))
 
 (defn suite
   "A test suite against a riemann repo in dir."
   [dir]
   {:before #(start-server dir)
-   :runs [;drop-tcp-event-run
-          ; async-tcp-rate-run
-          bulk-async-tcp-rate-run]
+   :runs [bulk-async-tcp-rate-run]
    :after stop-server})
-
-(defn suite'
-  "A dumb debugging suite"
-  [dir]
-  {:before #(prn "setup")
-   :after #(prn "teardown" %)
-   :runs [{:name "hi"
-           :n 1000
-           :threads 1
-           :before #(prn "before" %)
-           :after #(prn "after" %)
-           :f (fn [_] (Thread/sleep 10))}]})
 
 (defn -main
   [dir & versions]
